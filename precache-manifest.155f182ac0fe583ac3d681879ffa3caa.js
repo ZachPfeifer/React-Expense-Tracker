1e0a7ef543370fe0e1532ee93e5f99e78ad9dadb
Zach Pfeifer <mr.zman1@yahoo.com>
2020-02-26 17:50:54 -0700

    Updates

---
+++ precache-manifest.155f182ac0fe583ac3d681879ffa3caa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87cc94acdb8b91a71d1e1f14929f06aa",
+    "url": "/React-Expense-Tracker/index.html"
+  },
+  {
+    "revision": "c0cdb1da7863b9d4fbb1",
+    "url": "/React-Expense-Tracker/static/css/main.f1df2ecd.chunk.css"
+  },
+  {
+    "revision": "1b55844580bd9dc4fa0f",
+    "url": "/React-Expense-Tracker/static/js/2.4f0c3f8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Expense-Tracker/static/js/2.4f0c3f8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cdb1da7863b9d4fbb1",
+    "url": "/React-Expense-Tracker/static/js/main.caadba92.chunk.js"
+  },
+  {
+    "revision": "757a0a85e6d54291afb0",
+    "url": "/React-Expense-Tracker/static/js/runtime-main.aa438be8.js"
+  }
+]);
