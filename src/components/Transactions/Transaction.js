47cc5f4ad4409f492766c5635c2ef32bcccdfa1e
Zach Pfeifer <mr.zman1@yahoo.com>
2020-03-05 18:29:02 -0700

    Fixed delete button

--- src/components/Transactions/Transaction.js
+++ src/components/Transactions/Transaction.js
@@ -7,7 +7,9 @@ export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext)
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-      {transaction.text} <span>{sign}{numberWithCommas(Math.abs(transaction.amount))}</span><button className="delete-btn" onClick={() => deleteTransaction(transaction._id)}>X</button>
+      {transaction.text}
+      <span>{sign}{numberWithCommas(Math.abs(transaction.amount))}</span>
+      <button className="delete-btn" onClick={() => deleteTransaction(transaction._id)}>X</button>
     </li>
   )
 }
--- src/components/Transactions/TransactionList.js
+++ src/components/Transactions/TransactionList.js
@@ -12,11 +12,13 @@ const TransactionList = () => {
   return (
     <>
       <h3>History</h3>
-      <ul className="list">
-        {transactions.map(transaction => ((
-          <Transaction transaction={transaction} key={transaction._id} />
-        )))}
-      </ul>
+      <div className="list-container">
+        <ul className="list">
+          {transactions.map(transaction => ((
+            <Transaction transaction={transaction} key={transaction._id} />
+          )))}
+        </ul>
+      </div>
     </>
   )
 }
