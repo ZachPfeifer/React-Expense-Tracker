3a5858249ba61998c3eea2b535b2b5cbd9a2f499
ZachPfeifer <mr.zman1@yahoo.com>
2020-03-18 13:36:41 -0600

    Added Draw Icon

--- src/components/Nav/SideDrawer.js
+++ src/components/Nav/SideDrawer.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import UserIcon from '../../images/UserIcon.png'
 
 
 const sideDrawer = ({ show }) => {
+
+  
   let drawerClasses = 'side-drawer'
 
   if (show) {
@@ -11,8 +14,15 @@ const sideDrawer = ({ show }) => {
   return (
     <nav className={drawerClasses}>
       <ul>
+        <li className="mx-auto">
+        <li className="drawer__profile">
+          <p className="mx-auto">Welcome Guest User</p>
+          <img src={UserIcon} alt="Proile Pic"/>
+          <p><small className="mr-1">Sign in to see Account Details</small></p>
+        </li>
+        </li>
         <li><Link to="/React-Expense-Tracker/">Home</Link></li>
-        <li><Link to="/React-Expense-Tracker/account">Login</Link></li>
+        <li><Link to="/React-Expense-Tracker/account">Login/Register</Link></li>
       </ul>
     </nav>
   );
--- src/components/Nav/ToolBar.js
+++ src/components/Nav/ToolBar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 import DrawerToggleButton from './DrawerToggleButton'
 import withUser from '../../components/Login/withUser'
+import Logo from '../../images/logo.png'
 
 
 const ToolBar = (props) => {
@@ -25,7 +26,7 @@ const ToolBar = (props) => {
         <div className="toolbar__toggle-button">
           <DrawerToggleButton click={drawerClickHandler} />
         </div>
-        <div className="toolbar__logo"><Link to="/React-Expense-Tracker/">Transaction Tracker</Link></div>
+        <div className="toolbar__logo"><Link to="/React-Expense-Tracker/"><img src={Logo} alt="Logo"/> Transaction Tracker</Link></div>
         <div className="spacer"></div>
         <div className="toolbar__navigation-items">
           <ul>
